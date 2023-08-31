684c3d7283a2895ff52394bc7444022a6b37d06d
Teddy van Jerry <me@teddy-van-jerry.org>
2023-08-31 01:22:51 -0700

    Update Web UI (#5)

--- ui/static/script.js
+++ ui/static/script.js
@@ -1,23 +1,47 @@
-const commandInput = document.getElementById('commandInput');
-const runButton = document.getElementById('runButton');
-const outputArea = document.getElementById('outputArea');
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  "use strict";
 
-runButton.addEventListener('click', () => {
-    const command = commandInput.value;
-    
-    // Make an AJAX request to a Flask route for command execution
-    fetch('/execute_command', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: `command=${encodeURIComponent(command)}`,
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+const commandInput = document.getElementById("commandInput");
+const runButton = document.getElementById("runButton");
+const outputArea = document.getElementById("outputArea");
+
+runButton.addEventListener("click", () => {
+  const command = commandInput.value;
+
+  // Make an AJAX request to a Flask route for command execution
+  fetch("/execute_command", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `command=${encodeURIComponent(command)}`,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      outputArea.value = data.result;
     })
-    .then(response => response.json())
-    .then(data => {
-        outputArea.value = data.result;
-    })
-    .catch(error => {
-        outputArea.value = "An error occurred while processing the command.";
+    .catch((error) => {
+      outputArea.value = "An error occurred while processing the command.";
     });
 });
