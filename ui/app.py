100bbd07d34dcf2ccbb04f70472c59c45b93eab1
Teddy van Jerry <me@teddy-van-jerry.org>
2023-09-01 01:47:34 -0700

    Add Basic UI Functions (#5)

--- ui/app.py
+++ ui/app.py
@@ -1,20 +1,43 @@
 from flask import Flask, render_template, request, jsonify
+from pathlib import Path
 import subprocess
 
 app = Flask(__name__)
+
+source_path = Path(__file__).resolve()
+source_dir = source_path.parent
+root_dir = source_dir.parent
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/execute_command', methods=['POST'])
-def execute_command():
-    command = request.form.get('command')
+@app.route('/run_self_test', methods=['POST'])
+def run_self_test():
+    self_test_cmd = root_dir / "scripts" / "self_test.sh"
     try:
-        result = subprocess.check_output(command, shell=True, text=True)
-        return jsonify({'result': result})
+        result = subprocess.check_output(str(self_test_cmd), shell=True, text=True)
+        return jsonify({'result': result, 'success': True})
     except subprocess.CalledProcessError as e:
-        return jsonify({'result': f"Error: {e}"})
+        return jsonify({'result': f"Error: {e}", 'success': False})
+
+@app.route('/receive_udp', methods=['POST'])
+def receive_udp():
+    local_ip = request.form.get('localIP')
+    local_port = request.form.get('localPort')
+    remote_ip = request.form.get('remoteIP')
+    remote_port = request.form.get('remotePort')
+    timeout = request.form.get('timeout')
+
+    # First save config to file
+
+    # Run C++ program to receive UDP data
+
+    # Return result (change this to something more useful)
+    result = "Received UDP data. Local IP: {}, Local Port: {}, Remote IP: {}, Remote Port: {}, Timeout: {}".format(
+        local_ip, local_port, remote_ip, remote_port, timeout)
+
+    return jsonify({'result': result})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- ui/static/script.js
+++ ui/static/script.js
@@ -1,47 +1,100 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-  "use strict";
+  'use strict'
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll(".needs-validation");
+  const forms = document.querySelectorAll('.needs-validation');
 
   // Loop over them and prevent submission
-  Array.from(forms).forEach((form) => {
-    form.addEventListener(
-      "submit",
-      (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      var custom_valid = true;
+      if (form.getAttribute('id') == 'main_form') {
+        custom_valid = validateEncoderParallelism();
+      }
+      if (!form.checkValidity() || !custom_valid) {
+        event.preventDefault();
+        event.stopPropagation();
+        form.classList.add('was-validated');
+      } else {
+        form.classList.remove('was-validated');
+      }
+    }, false)
+  })
 })();
 
-const commandInput = document.getElementById("commandInput");
-const runButton = document.getElementById("runButton");
-const outputArea = document.getElementById("outputArea");
+// const commandInput = document.getElementById("commandInput");
+// const runButton = document.getElementById("runButton");
+// const outputArea = document.getElementById("outputArea");
 
-runButton.addEventListener("click", () => {
-  const command = commandInput.value;
+// runButton.addEventListener("click", () => {
+//   const command = commandInput.value;
 
-  // Make an AJAX request to a Flask route for command execution
-  fetch("/execute_command", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: `command=${encodeURIComponent(command)}`,
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      outputArea.value = data.result;
-    })
-    .catch((error) => {
-      outputArea.value = "An error occurred while processing the command.";
+//   // Make an AJAX request to a Flask route for command execution
+//   fetch("/execute_command", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//     body: `command=${encodeURIComponent(command)}`,
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       outputArea.value = data.result;
+//     })
+//     .catch((error) => {
+//       outputArea.value = "An error occurred while processing the command.";
+//     });
+// });
+
+$(document).ready(function () {
+  $("#receive-udp-button").click(function () {
+    const form = $(this).closest("form")[0]; // Get the parent form element
+    if (!form.checkValidity()) {
+      form.classList.add("was-validated");
+      return;
+    }
+    $("#run-loading-spinner").removeClass("visually-hidden");
+    $.ajax({
+      type: "POST",
+      url: "/receive_udp",
+      data: {
+        localIP: $("#local-ip").val(),
+        localPort: $("#local-port").val(),
+        remoteIP: $("#remote-ip").val(),
+        remotePort: $("#remote-port").val(),
+        timeout: $("#timeout").val(),
+      },
+      success: function (response) {
+        var result = response.result;
+        $("#terminal-output").val(result);
+      },
+      error: function (response) {
+        var result = response.responseJSON.result;
+        $("#terminal-output").val(result);
+      },
+      complete: function() {
+        $("#run-loading-spinner").addClass("visually-hidden");
+      }
     });
+  });
+
+  $("#run-self-test-button").click(function () {
+    $("#run-loading-spinner").removeClass("visually-hidden");
+    $.ajax({
+      type: "POST",
+      url: "/run_self_test",
+      success: function (response) {
+        var result = response.result;
+        $("#terminal-output").val(result);
+      },
+      error: function (response) {
+        var result = response.responseJSON.result;
+        $("#terminal-output").val(result);
+      },
+      complete: function() {
+        $("#run-loading-spinner").addClass("visually-hidden");
+      }
+    });
+  });
 });
